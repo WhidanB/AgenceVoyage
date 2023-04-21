347fba4303198cba848ec1fcbed32c138182a6ee
WhidanB <danyphone95@gmail.com>
2023-04-21 13:44:51 +0200

    ajout photo et caroussel 	new file:   images/Bouthan/bradford-zak-hoCBS8FbFPQ-unsplash.jpg 	new file:   images/Bouthan/daniel-grandfield-_pvYscWJ7ao-unsplash.jpg 	new file:   images/Bouthan/darshan-chudasama-Fu7Se62jOZ8-unsplash.jpg 	new file:   images/Bouthan/raimond-klavins-JqT2Wp5S0Dk-unsplash.jpg 	modified:   index.html 	modified:   index.js 	modified:   style.css

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const header = document.querySelector("header");
-console.log(header);
 
 window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-
   if (window.scrollY > 100) {
     header.classList.remove("entete");
     header.classList.add("headerscroll");
@@ -12,4 +9,37 @@ window.addEventListener("scroll", () => {
     header.classList.remove("headerscroll");
     header.classList.add("entete");
   }
+});
+
+const slider = document.querySelector(".slider");
+const vectorLeft = document.querySelector(".vector1");
+const vectorRight = document.querySelector(".vector2");
+let slideArray = document.getElementsByClassName("slide");
+let active = slideArray;
+let trans = 0;
+
+vectorLeft.addEventListener("click", () => {
+  if (trans == 0) {
+    trans += 80;
+    slider.style.transform = "translate(-" + trans + "%,0)";
+  } else if (trans <= 80) {
+    trans -= 20;
+    slider.style.transform = "translate(-" + trans + "%,0)";
+  }
+  // if (active == slideArray.length) {
+  // console.log(slideArray.length);
+  console.log(trans);
+});
+
+vectorRight.addEventListener("click", () => {
+  if (trans < 80) {
+    trans += 20;
+    slider.style.transform = "translate(-" + trans + "%,0)";
+  } else if (trans >= 80) {
+    slider.style.transform = "translateX(0%)";
+    trans = 0;
+  }
+  // if (active == slideArray.length) {
+  // console.log(slideArray.length);
+  console.log(trans);
 });
