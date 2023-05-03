ab2a286aa56121eda87ce6c64f2d4e5d7fb5adea
WhidanB <danyphone95@gmail.com>
2023-05-03 13:54:45 +0200

    page login + ajout liens header 	modified:   index.html 	modified:   index.js 	new file:   login.html 	modified:   style.css

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const header = document.querySelector("header");
 
-var input = document.querySelectorAll("input");
-for (i = 0; i < input.length; i++) {
-  input[i].setAttribute("size", input[i].getAttribute("placeholder").length);
-}
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     header.classList.remove("entete");
